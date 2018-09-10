65f9a6b1d4d09b84139316a3884071b3ed354d77
Aditya <nawandaraditya60@gmail.com>
2018-09-10 14:13:00 +0530

    proof of work

--- Blockchain/block.test.js
+++ Blockchain/block.test.js
@@ -1,10 +1,9 @@
 const Block = require('./block');
+//const { DIFFICULTY } = require('../config');
 
-describe('Block', () => 
-{
+describe('Block', () => {
     let data, lastBlock, block;
-    beforeEach(()=>
-    {
+    beforeEach(() => {
         data = 'bar';
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
@@ -14,8 +13,22 @@ describe('Block', () =>
         expect(block.data).toEqual(data);
     });
 
-    it('sets the `lastHash` to match the hash of the last block', () =>{
+    it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash)
     });
 
- });
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('lowers the difficulty for slowly mined blocks', () => 
+    {
+        expect(Block.adjustDifficulty(block,block.timestamp+360000)).toEqual(block.difficulty-1);
+    });
+
+    it('raises the difficulty for quickly mined blocks', () => 
+    {
+        expect(Block.adjustDifficulty(block, block.timestamp+1)).toEqual(block.difficulty + 1);
+    });
+
+});
--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,8 @@
-const Block = require('./block')
+const Blockchain = require('./Blockchain/index');
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+const bc = new Blockchain();
 
-console.log(fooBlock.toString());
+for(let i=0; i<10; i++)
+{
+    console.log(bc.addBlock(`foo${i}`).toString());
+}
